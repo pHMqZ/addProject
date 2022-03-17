6b7e08250df8ff95b527eb75ef972498be992916
Phillip Marques <phillipmarq@hotmail.com>
2022-03-16 21:13:12 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1> RESTApi - Escola</h1>
+
+<h3> Projeto para implemantação de RESTApi de Escolas - .add</h3>
+
+<p> Aplicação possue 4 entidades, sendo elas:</p>
+<ol>
+    <li> Aluno;</li>
+    <li> Turma;</li>
+    <li> Escola;</li>
+    <li> Endereço.</li>
+</ol>
+
+<p> Para todas as entidades a partir de seus controlleres, é possivel realizar os seguintes métodos:<p>
+<ol>
+    <li> Create;</li>
+    <li> Get all;</li>
+    <li> Get By Id;</li>
+    <li> Update;</li>
+    <li> Remove.</li>
+</ol>
+
+<h3> Pré-requisitos para rodar o projeto:</h3>
+<ol>
+    <li> Java 11 ou superior;</li>
+    <li> Maven 3.6.3 ou superior;</li>
+    <li> IDE de sua preferência</li>
+    <li> MySQL server ou Xampp.</li>
+</ol>
+
+<h3> Como rodar a aplicação:</h3>
+<ol> Pela IDE:
+    <li> Garantir que o server MySQL está rodando ou ativar utilizando o programa Xampp;</li>
+    <li> No nome da API, clicar com o botão direito, ir até run as e por final clicar na opção 'Spring Boot App';</li>
+    <li>Aguardar a API terminar de inicializar e realizar as requisições</li>
+</ol>
+<ol> Pelo CMD:
+    <li> Garantir que o server MySQL está rodando ou ativar utilizando o programa Xampp;</li>
+    <li> Iniciar o prompt de comando na pasta onde está a API</li>
+    <li> Rodar o seguinte comando:</li>
+    ``` mvn spring-boot:run```
+    <li> Aguardar a API terminar e realizar as requisições</li>
+</ol>
+
+Projeto realizado por [Phillip Marques](https://www.linkedin.com/in/phillip-marques-201832161/ "Phillip Marques")
--- src/main/java/com/pms/add/controller/TurmaController.java
+++ src/main/java/com/pms/add/controller/TurmaController.java
@@ -2,6 +2,9 @@ package com.pms.add.controller;
 
 import java.util.List;
 
+import com.pms.add.model.Turma;
+import com.pms.add.service.TurmaService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,40 +17,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pms.add.model.Escola;
-import com.pms.add.model.Turma;
-import com.pms.add.service.EscolaService;
-import com.pms.add.service.TurmaService;
-
 @RestController
 @RequestMapping("/api")
 public class TurmaController {
-	
+
 	@Autowired
 	private TurmaService turServ;
-	
+
 	@GetMapping("/turmas")
-	public List<Turma> listarTurmas(){
+	public List<Turma> listarTurmas() {
 		return turServ.getAll();
 	}
-	
+
 	@GetMapping("/turmas/{id}")
-	public ResponseEntity<?> listarTurmasPorId(@PathVariable Long id){
+	public ResponseEntity<?> listarTurmasPorId(@PathVariable Long id) {
 		return turServ.getTurmaById(id);
 	}
-	
+
 	@PostMapping("/turmas/novo")
 	public Turma salvarTurma(@RequestBody Turma tur) {
 		return turServ.createTurma(tur);
 	}
-	
+
 	@PutMapping("/turmas/editar/{id}")
 	public Turma atualizarTurma(@PathVariable Long id, @RequestBody Turma tur) {
 		return turServ.uptadeTurma(id, tur);
 	}
-	
+
 	@DeleteMapping("/turmas/remover/{id}")
-	public ResponseEntity<?> deletarEscola(@PathVariable Long id){
+	public ResponseEntity<?> deletarEscola(@PathVariable Long id) {
 		turServ.removeTurma(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
